d685bbff62449bdd246cbd22831ee110ab12ca63
Aashray <aashray@uw.edu>
2019-12-30 15:15:37 -0800

    reworking cryptopals solutions to use C++ instead of python, avoiding any unnecessary libraries and manipulating all characters directly

---
+++ Set1/HexToB64.h
@@ -0,0 +1,5 @@
+// string of b64 characters, index of each character
+// is the associated numerical value of the character
+const std::string b64table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+std::string HexToB64(std::string);
